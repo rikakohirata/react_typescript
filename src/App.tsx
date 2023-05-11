09ad33f90d3b1e45f8676e0756b55d7690274101
rikakohirata <rikako.hirata88@gmail.com>
2023-05-11 19:36:30 +0900

    いいねボタンの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,8 +18,29 @@ function App() {
         >
           Learn React
         </a>
+      </header> */}
+      <header className="App-header">
+        {/* いいねボタンを表示 */}
+        <LinkButton />
       </header>
     </div>
+  );
+}
+
+// いいねボタン
+function LinkButton() {
+  // useState関数により、カウント数の状態をReactに管理させる
+  // useStateの戻り値を、countとsetCountの2つの変数に代入
+  // countに999, setCountにcountの値を変更するhandleClick関数を代入
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    // クリック毎にhandleClick関数が実行される
+    <span className="likeButton" onClick={handleClick}>
+      ♥ {count}
+    </span>
   );
 }
 
